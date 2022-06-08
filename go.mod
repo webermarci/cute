817f9f00b5441c46898b382c48e7a13255726adb
webermarci <webermarton@gmail.com>
2022-06-08 21:14:15 +0200

    Add workers to limit parallel task, different approach for command input, removed formatting

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/webermarci/cute
 
 go 1.18
+
+require (
+	github.com/akamensky/argparse v1.3.1
+	github.com/alitto/pond v1.8.0
+)
--- main.go
+++ main.go
@@ -1,111 +1,205 @@
 package main
 
 import (
+	"bufio"
 	"encoding/json"
-	"flag"
 	"fmt"
+	"io"
 	"log"
+	"os"
 	"os/exec"
+	"runtime"
+	"strconv"
 	"strings"
-	"sync"
 	"time"
+	"unicode"
+
+	"github.com/akamensky/argparse"
+	"github.com/alitto/pond"
 )
+
+type CommandGroup struct {
+	Commands []Command
+}
+
+type Command struct {
+	Command string
+	Alias   string
+}
 
 type Result struct {
-	Command  string        `json:"command"`
-	Output   string        `json:"output"`
-	Duration time.Duration `json:"duration"`
+	Command  string
+	Alias    string
+	Output   string
+	Duration time.Duration
 }
 
-var (
-	verbose  bool
-	parallel bool
-	format   string
-	shell    string
-	commands []string
-)
+func parseGroups(input string) map[string]string {
+	lastQuote := rune(0)
+	f := func(c rune) bool {
+		switch {
+		case c == lastQuote:
+			lastQuote = rune(0)
+			return false
+		case lastQuote != rune(0):
+			return false
+		case unicode.In(c, unicode.Quotation_Mark):
+			lastQuote = c
+			return false
+		default:
+			return unicode.IsSpace(c)
 
-func parseArgs() {
-	flag.BoolVar(&verbose, "v", false, "Verbose mode")
-	flag.BoolVar(&parallel, "p", false, "Execute the commands in parallel")
-	flag.StringVar(&format, "f", "json", "Format of the output")
-	flag.StringVar(&shell, "s", "sh", "Shell to use")
-	flag.Parse()
-	commands = flag.Args()
-
-	switch format {
-	case "json", "csv", "logfmt":
-	default:
-		log.Fatal("Invalid format type")
+		}
 	}
+
+	items := strings.FieldsFunc(input, f)
+
+	m := make(map[string]string)
+	for _, item := range items {
+		x := strings.Split(item, "=")
+		m[x[0]] = strings.Trim(x[1], "\"")
+	}
+
+	return m
 }
 
-func execute(command string) Result {
+func execute(shell string, command Command) Result {
 	start := time.Now()
 
-	out, err := exec.Command(shell, "-c", command).Output()
+	out, err := exec.Command(shell, "-c", command.Command).Output()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	result := Result{
-		Command:  command,
+	return Result{
+		Command:  command.Command,
+		Alias:    command.Alias,
 		Output:   strings.TrimSpace(string(out)),
 		Duration: time.Since(start),
 	}
-
-	if verbose {
-		log.Printf("\"%s\" returned %s in %s", command, result.Output, result.Duration)
-	}
-
-	return result
 }
 
 func main() {
-	start := time.Now()
+	description := `You need to pipe the commands into it:
+	    "commands.txt | cute" or "cute < commands.txt"
+	    
+	    The commands.txt file should contain lines of command groups like so:
+	    first="echo hello" second="echo world"
+	    thrid="echo bye"
 
-	parseArgs()
-	var results []Result = make([]Result, len(commands))
+	    Command groups may contain one or more commands. Command groups are executed in parallel, but commands in a group are sync.
+	`
 
-	if parallel {
-		wg := sync.WaitGroup{}
-		wg.Add(len(commands))
-		for i, command := range commands {
-			go func(i int, cmd string) {
-				results[i] = execute(cmd)
-				wg.Done()
-			}(i, command)
-		}
-		wg.Wait()
-	} else {
-		for i, command := range commands {
-			results[i] = execute(command)
-		}
+	parser := argparse.NewParser("cute", description)
+	version := parser.Flag("V", "version",
+		&argparse.Options{Help: "Print version"})
+	verbose := parser.Flag("v", "verbose",
+		&argparse.Options{Help: "Verbose mode"})
+	n := parser.Int("n", "parallel-tasks",
+		&argparse.Options{Default: runtime.NumCPU(), Help: "Number of parallel tasks"})
+	shell := parser.String("s", "shell",
+		&argparse.Options{Default: "bash", Help: "Shell to execute the commands with"})
+
+	err := parser.Parse(os.Args)
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	if verbose {
+	if *version {
+		fmt.Println("cute - v0.3")
+		return
+	}
+
+	info, err := os.Stdin.Stat()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if info.Mode()&os.ModeCharDevice != 0 || info.Size() <= 0 {
+		log.Fatal("Nothing is piped into")
+		return
+	}
+
+	reader := bufio.NewReader(os.Stdin)
+	var pipe []rune
+
+	for {
+		input, _, err := reader.ReadRune()
+		if err != nil && err == io.EOF {
+			break
+		}
+		pipe = append(pipe, input)
+	}
+
+	if len(pipe) == 0 {
+		log.Fatal("Nothing is piped into")
+	}
+
+	var groups []CommandGroup
+	commandCounter := 0
+
+	for _, groupString := range strings.Split(string(pipe), "\n") {
+		group := CommandGroup{}
+		parsed := parseGroups(groupString)
+		for alias, command := range parsed {
+			group.Commands = append(group.Commands, Command{
+				Alias:   alias,
+				Command: command,
+			})
+			commandCounter++
+		}
+		groups = append(groups, group)
+	}
+
+	if len(groups) == 0 {
+		log.Fatal("Failed to parse any command groups")
+	}
+
+	pool := pond.New(*n, commandCounter)
+	groupResults := make([]map[string]any, len(groups))
+	start := time.Now()
+
+	for i, g := range groups {
+		index := i
+		group := g
+
+		pool.Submit(func() {
+			results := make([]Result, len(group.Commands))
+			for j, cmd := range group.Commands {
+				results[j] = execute(*shell, cmd)
+
+				if *verbose {
+					log.Printf("%s=\"%s\" returned %s in %s",
+						cmd.Alias, cmd.Command, results[j].Output, results[j].Duration)
+				}
+			}
+
+			groupResult := make(map[string]any)
+			for _, result := range results {
+				parsedFloat, err := strconv.ParseFloat(result.Output, 64)
+				if err == nil {
+					groupResult[result.Alias] = parsedFloat
+				} else {
+					groupResult[result.Alias] = result.Output
+				}
+
+				groupResult[result.Alias+"_duration"] = int64(result.Duration)
+			}
+			groupResults[index] = groupResult
+		})
+	}
+
+	pool.StopAndWait()
+
+	if *verbose {
 		log.Printf("All finished in %s\n", time.Since(start))
 	}
 
-	switch format {
-	case "json":
-		for _, result := range results {
-			bytes, err := json.Marshal(result)
-			if err != nil {
-				log.Fatal(err)
-			}
-			fmt.Println(string(bytes))
+	for _, result := range groupResults {
+		data, err := json.Marshal(result)
+		if err != nil {
+			log.Fatal(err)
 		}
-
-	case "csv":
-		fmt.Println("command,output,duration")
-		for _, result := range results {
-			fmt.Printf("%s,%s,%d\n", result.Command, result.Output, result.Duration)
-		}
-
-	case "logfmt":
-		for _, result := range results {
-			fmt.Printf("command=\"%s\" output=\"%s\" duration=%d\n", result.Command, result.Output, result.Duration)
-		}
+		fmt.Println(string(data))
 	}
 }
